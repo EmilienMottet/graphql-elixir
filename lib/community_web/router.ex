015580110bdce46d38e43c814df773770e9ba323
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2018-09-04 21:30:42 +0200

    step 2

--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -7,5 +7,12 @@ defmodule CommunityWeb.Router do
 
   scope "/api", CommunityWeb do
     pipe_through :api
+
   end
+
+  forward "/graphiql", Absinthe.Plug.GraphiQL,
+    schema: CommunityWeb.Schema,
+    interface: :simple,
+    context: %{pubsub: CommunityWeb.Endpoint}
+
 end
